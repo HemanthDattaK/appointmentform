fd3c12766113851433f317edd53885438b74d779
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-08-19 13:10:33 +0530

    Update settings.py

--- appoint/settings.py
+++ appoint/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'form/static')]  # Changed to a list
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
