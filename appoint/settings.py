7eb52a067548de005cff71f87c145c9e24296f18
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-08-19 13:08:42 +0530

    Update settings.py

--- appoint/settings.py
+++ appoint/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'form/static')]  # Changed to a list
 STATIC_URL = '/static/'
 
