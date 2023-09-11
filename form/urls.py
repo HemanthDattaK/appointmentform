5834c5ac4e8c0c1388e877a7870f246f7c6816e6
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-09-11 13:16:30 +0530

    Update urls.py

--- form/urls.py
+++ form/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_user, name='login'),
-    path('display/', views.contact_display_view, name='display'),
+    path('display/', login_required(views.contact_display_view), name='display'),
     path('', views.contact_view, name='contact'),
     path('success/', views.success, name='success'),
     path('set/', views.set_appointment_times, name='set_times'),
