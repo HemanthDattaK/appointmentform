f25a3ba7c3aefbe6b06c8f2e72688b9771d24063
Hemanth datta <99961202+Henryhose@users.noreply.github.com>
2023-09-28 11:06:17 +0530

    Update urls.py

--- form/urls.py
+++ form/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('set/', views.set_appointment_times, name='set_times'),
     path('after/', views.afterview, name='after'),  # Requires authentication
     path('delete_contact/', views.contact_display_view, name='contact_delete'),
+    path('generate_token/', views.generate_jwt_token, name='generate_jwt_token'),
+
 ]
